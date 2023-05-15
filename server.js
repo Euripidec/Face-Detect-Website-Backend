1799cfcf85f6eae87725f2f38b7aefab911fad1f
Euripidec <111103974+Euripidec@users.noreply.github.com>
2023-05-14 22:23:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const image = require('./controllers/image')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : process.env.DATABASE_HOST,
     port : 5432,
-    user : 'postgres',
-    password : 'test',
-    database : 'postgres'
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PW,
+    database : process.env.DATABASE_DB,
   }
 });
 
