4d196bb75aa5122786f73f320d402da954d0b5c6
Euripidec <111103974+Euripidec@users.noreply.github.com>
2023-05-14 21:35:51 -0400

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,4 @@
-const returnClarifaiRequestOptions = (imageUrl) => {
-  // Your PAT (Personal Access Token) can be found in the portal under Authentification
-  const PAT = "9fbc374bbe63427f9baea6aee65a24eb";
-  // Specify the correct user_id/app_id pairings
-  // Since you're making inferences outside your app's scope
-  const USER_ID = "superboi";
-  const APP_ID = "Test";
-  // Change these to whatever model and image URL you want to use
-  // const MODEL_ID = "face-detection";
-  // const MODEL_VERSION_ID = "aa7f35c01e0642fda5cf400f543e7c40";
-  const IMAGE_URL = imageUrl;
-  
+
 const handleImage = (req,res,db)=>{
 	const { id } = req.body;
 	db('users').where('id','=', id)
